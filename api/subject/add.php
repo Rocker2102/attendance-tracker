93a796a7b637752c9eb9cd3cf6eab6e2a04c2ccc
Ankush Yadav <ankushyadav9302@gmail.com>
2020-11-06 13:52:31 +0530

    [ADD] api/subject/add.php
    adds api endpint to add subjects

---
+++ api/subject/add.php
@@ -0,0 +1,67 @@
+<?php
+    require "../config/headers.php";
+    require "../config/operations.php";
+    require "../config/error_def.php";
+
+    $allowed_req_methods = ["POST"];
+    $error = new Error_Definitions;
+
+    if (!check_request_method($allowed_req_methods)) {
+        send_response(405, $error->data_error(1));
+    }
+
+    require "../config/preflight.php";
+
+    $access_token = get_access_token();
+    if (!$access_token) {
+        send_response(401, $error->custom("ERR_API_AUTH", "Access Token missing"));
+    }
+
+    $data = $_POST;
+
+    if ($data == null || empty($data)) {
+        send_response(400, $error->data_error(2));
+    }
+
+    $required_keys = ["subject_id", "name"];
+    $valid_keys = [];
+
+    $validator = new Validate_Data;
+    $data = $validator->verify($data, $required_keys, $valid_keys);
+
+    if (!$data) {
+        send_response(400, $error->form_error(1));
+    }
+
+    $data["subject_id"] = strtoupper($data["subject_id"]);
+    Utility::escape_array($data);
+
+    require "../config/database.php";
+    $database = new Database;
+    $connect = $database->get_connect_var();
+
+    if (!Authenticate::verify_access_token($connect, $access_token)) {
+        send_response(401, $error->custom("ERR_API_TOKEN", "Token invalid or expired!"));
+    }
+
+    $query = new Build_Query("subjects", "insert");
+    $query->set_columns(array_keys($data));
+    $query->set_values(array_values($data));
+
+    $connect->query($query->get_query());
+
+    if (mysqli_affected_rows($connect) == 1) {
+        send_response(201, array(
+            "error" => false,
+            "message" => "Subject added",
+            "data" => array(
+                "code" => $data["subject_id"],
+                "name" => $data["name"]
+            )
+        ));
+    } else {
+        send_response(409, $error->db_error(1), array(
+            "info" => "Check for duplicate subject code"
+        ));
+    }
+?>
