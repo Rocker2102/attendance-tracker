aabffcbbe2a301c324c7e11229da892390aab6a2
Ankush Yadav <ankushyadav9302@gmail.com>
2020-11-08 21:31:19 +0530

    [UPDATE] api/config/headers.php
    updates global access-control-allow-methods

--- api/config/headers.php
+++ api/config/headers.php
@@ -1,7 +1,7 @@
 <?php
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
-    header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS");
+    header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, X-Access-Token");
 ?>
